8442644d70a25d7e486780e30969c2e1bf6140d5
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2024-02-23 16:13:13 +0100

    Added validation & added endpoints for orders API

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { db, Order } from './../db';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class OrdersService {
@@ -9,5 +10,27 @@ export class OrdersService {
 
   public getById(id: Order['id']): Order | null {
     return db.orders.find((p) => p.id === id);
+  }
+
+  public delete(id: string): void {
+    db.orders = db.orders.filter((order) => order.id !== id);
+  }
+
+  public create(orderData: Omit<Order, 'id' | 'productId'>): Order {
+    const newOrder = { ...orderData, id: uuidv4(), productId: uuidv4() };
+    db.orders.push(newOrder);
+    return newOrder;
+  }
+
+  public updateById(
+    id: Order['id'],
+    orderData: Omit<Order, 'id' | 'productId'>,
+  ): void {
+    db.orders = db.orders.map((order) => {
+      if (order.id === id) {
+        return { ...order, ...orderData };
+      }
+      return order;
+    });
   }
 }
