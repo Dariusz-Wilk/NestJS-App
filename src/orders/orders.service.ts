71653b892264584b840498f6b9fe25f5a486cc5e
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2024-02-28 18:27:45 +0100

    Connected app to the SQL data base

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,36 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { db, Order } from './../db';
+import { Order } from '@prisma/client';
+import { PrismaService } from 'src/shared/services/prisma.service';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class OrdersService {
-  public getAll(): Order[] {
-    return db.orders;
+  constructor(private prismaService: PrismaService) {}
+  public getAll(): Promise<Order[]> {
+    return this.prismaService.order.findMany();
   }
 
-  public getById(id: Order['id']): Order | null {
-    return db.orders.find((p) => p.id === id);
+  public getById(id: Order['id']): Promise<Order | null> {
+    return this.prismaService.order.findUnique({ where: { id } });
   }
 
-  public delete(id: string): void {
-    db.orders = db.orders.filter((order) => order.id !== id);
+  public delete(id: string): Promise<Order> {
+    return this.prismaService.order.delete({ where: { id } });
   }
 
-  public create(orderData: Omit<Order, 'id' | 'productId'>): Order {
-    const newOrder = { ...orderData, id: uuidv4(), productId: uuidv4() };
-    db.orders.push(newOrder);
-    return newOrder;
+  public create(
+    orderData: Omit<Order, 'id' | 'productId' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Order> {
+    return this.prismaService.order.create({
+      data: { ...orderData, productId: uuidv4() },
+    });
   }
 
   public updateById(
     id: Order['id'],
-    orderData: Omit<Order, 'id' | 'productId'>,
-  ): void {
-    db.orders = db.orders.map((order) => {
-      if (order.id === id) {
-        return { ...order, ...orderData };
-      }
-      return order;
+    orderData: Omit<Order, 'id' | 'productId' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Order> {
+    return this.prismaService.order.update({
+      where: { id },
+      data: { ...orderData, productId: uuidv4() },
     });
   }
 }
